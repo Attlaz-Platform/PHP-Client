f1f73a93b83a361dc038d966c72bdf92947f89ac
Stijn Duynslaeger <stijn@echron.be>
2018-08-07 19:29:06 +0200

    Store access token (encrypted) to reuse on upcoming requests

---
+++ src/Helper/TokenStorage.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace Attlaz\Helper;
+
+use League\OAuth2\Client\Token\AccessToken;
+
+class TokenStorage
+{
+    public static function loadAccessToken(string $clientId, string $clientSecret)
+    {
+        $data = self::getData($clientId, $clientSecret);
+
+        $encrypt_method = $data['encrypt_method'];
+        $key = $data['key'];
+        $iv = $data['iv'];
+        $file = $data['file'];
+
+        if (file_exists($file) && is_readable($file)) {
+            $content = openssl_decrypt(base64_decode(\file_get_contents($file)), $encrypt_method, $key, 0, $iv);
+            $accessToken = unserialize($content);
+
+            if (!is_null($accessToken) && is_a($accessToken, AccessToken::class) && !$accessToken->hasExpired()) {
+                return $accessToken;
+            }
+        }
+
+        return null;
+    }
+
+    private static function getData(string $clientId, string $clientSecret): array
+    {
+        $data = [
+            'encrypt_method' => "AES-256-CBC",
+            'key'            => hash('sha256', $clientId . '-' . $clientSecret),
+            'iv'             => substr(hash('sha256', $clientSecret), 0, 16),
+            'file'           => '_token_' . \hash('sha512', $clientId . $clientSecret),
+        ];
+
+        return $data;
+    }
+
+    public static function saveAccessToken(AccessToken $accessToken, string $clientId, string $clientSecret)
+    {
+        $data = self::getData($clientId, $clientSecret);
+
+        $encrypt_method = $data['encrypt_method'];
+        $key = $data['key'];
+        $iv = $data['iv'];
+        $file = $data['file'];
+
+        $content = base64_encode(openssl_encrypt(\serialize($accessToken), $encrypt_method, $key, 0, $iv));
+
+        file_put_contents($file, $content);
+    }
+}
