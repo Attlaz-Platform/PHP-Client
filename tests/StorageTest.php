3e4dae59261b141b7f283a42270cb99b38f10b5f
Stijn Duynslaeger <stijn@echron.com>
2022-08-23 21:43:58 +0100

    Improve storage test

--- tests/StorageTest.php
+++ tests/StorageTest.php
@@ -18,23 +18,27 @@ class StorageTest extends TestCase
 
     public function testWriteItem()
     {
-        $client = new \Attlaz\Client(\getenv('api_client_id'), \getenv('api_client_secret'));
+        $client = new Client(\getenv('api_client_id'), \getenv('api_client_secret'));
 //$client->setEndPoint('https://api2.attlaz.com');
         $client->setEndPoint('https://api.attlaz.com/beta/');
 //$client = new \Attlaz\Client('http://10.0.75.1:8080/', '6as&01LW!iVe!wO7Guv%5#MlfZ2SJgSG', '#zqtn*4IKcx7iNM4bNvc$XU@H27prch8');
-        $client->enableDebug();
 
-        $item = new StorageItem();
-        $item->key = 'randomkey';
-        $item->value = 'randomvalue';
-        $set = $client->getStorageEndpoint()->setItem(61, 'cache', $item);
-        $this->assertTrue($set);
+        $values = ['randomvalue', ['a' => 'yeah', 'x' => 12.3], null, true, 12.6, ['a', 'b', 1, 10]];
+        foreach ($values as $value) {
+            $client->enableDebug();
 
-        $v = $client->getStorageEndpoint()->getItem(61, 'cache', $item->key);
+            $item = new StorageItem();
+            $item->key = 'randomkey-' . \rand();
+            $item->value = $value;
+            $set = $client->getStorageEndpoint()->setItem('61', 'cache', $item);
+            $this->assertTrue($set);
 
-        \var_dump($v->value);
+            $v = $client->getStorageEndpoint()->getItem('61', 'cache', $item->key);
 
-        $this->assertEquals($item->value, $v->value);
+
+            $this->assertEquals($item->value, $v->value);
+        }
+
 
     }
 }
